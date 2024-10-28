e53765b09ff217ee08fef301f4aa4fc967a68eb1
devosubj491 <devosubj491@ed.amdsb.ca>
2024-10-28 10:46:19 -0400

    Add project files.

---
+++ AdventureGameSummative/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace AdventureGameSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.optionLabel1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.optionLabel2 = new System.Windows.Forms.Label();
+            this.optionButton2 = new System.Windows.Forms.Button();
+            this.optionButton1 = new System.Windows.Forms.Button();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // optionLabel1
+            // 
+            this.optionLabel1.BackColor = System.Drawing.Color.DarkGreen;
+            this.optionLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel1.Location = new System.Drawing.Point(109, 281);
+            this.optionLabel1.Name = "optionLabel1";
+            this.optionLabel1.Size = new System.Drawing.Size(369, 28);
+            this.optionLabel1.TabIndex = 1;
+            this.optionLabel1.Text = "optionLabel1";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Black;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(0, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(492, 49);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "outputLabel";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // optionLabel2
+            // 
+            this.optionLabel2.BackColor = System.Drawing.Color.DarkRed;
+            this.optionLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel2.Location = new System.Drawing.Point(109, 338);
+            this.optionLabel2.Name = "optionLabel2";
+            this.optionLabel2.Size = new System.Drawing.Size(369, 28);
+            this.optionLabel2.TabIndex = 2;
+            this.optionLabel2.Text = "optionLabel2";
+            // 
+            // optionButton2
+            // 
+            this.optionButton2.AutoSize = true;
+            this.optionButton2.BackColor = System.Drawing.Color.Transparent;
+            this.optionButton2.BackgroundImage = global::AdventureGameSummative.Properties.Resources.arrowRight;
+            this.optionButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.optionButton2.FlatAppearance.BorderSize = 0;
+            this.optionButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.optionButton2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.optionButton2.Location = new System.Drawing.Point(48, 326);
+            this.optionButton2.Name = "optionButton2";
+            this.optionButton2.Size = new System.Drawing.Size(55, 55);
+            this.optionButton2.TabIndex = 4;
+            this.optionButton2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.optionButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.optionButton2.UseVisualStyleBackColor = false;
+            this.optionButton2.Click += new System.EventHandler(this.optionButton2_Click);
+            // 
+            // optionButton1
+            // 
+            this.optionButton1.AutoSize = true;
+            this.optionButton1.BackColor = System.Drawing.Color.Transparent;
+            this.optionButton1.BackgroundImage = global::AdventureGameSummative.Properties.Resources.arrowRight;
+            this.optionButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.optionButton1.FlatAppearance.BorderSize = 0;
+            this.optionButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.optionButton1.ForeColor = System.Drawing.Color.Transparent;
+            this.optionButton1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.optionButton1.Location = new System.Drawing.Point(48, 265);
+            this.optionButton1.Name = "optionButton1";
+            this.optionButton1.Size = new System.Drawing.Size(55, 55);
+            this.optionButton1.TabIndex = 3;
+            this.optionButton1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.optionButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.optionButton1.UseVisualStyleBackColor = false;
+            this.optionButton1.Click += new System.EventHandler(this.optionButton1_Click);
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(12, 52);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(466, 195);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox.TabIndex = 5;
+            this.imageBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(490, 390);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.optionButton2);
+            this.Controls.Add(this.optionButton1);
+            this.Controls.Add(this.optionLabel2);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.optionLabel1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Going To Work";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label optionLabel1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label optionLabel2;
+        private System.Windows.Forms.Button optionButton1;
+        private System.Windows.Forms.Button optionButton2;
+        private System.Windows.Forms.PictureBox imageBox;
+    }
+}
+
---
+++ AdventureGameSummative/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace AdventureGameSummative
+{
+    public partial class Form1 : Form
+    {
+        //Track the page of the story you are at
+        int page = 1;
+        SoundPlayer soundPlayer = new SoundPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //display initial message and options
+            DisplayPage();
+        }
+
+        private void optionButton1_Click(object sender, EventArgs e)
+        {
+            //Check page, then flip based on if you selected Option 1
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 3)
+            {
+                page = 1;
+            }
+            else if (page == 4)
+            {
+                page = 5;
+            }
+            else if (page == 5)
+            {
+                page = 1;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 7)
+            {
+                page = 8;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                page = 1;
+            }
+            else if (page == 10)
+            {
+                page = 11;
+            }
+            else if (page == 11)
+            {
+                page = 13;
+            }
+            else if (page == 12)
+            {
+                page = 1;
+            }
+            else if (page == 13)
+            {
+                page = 1;
+            }
+            else if (page == 14)
+            {
+                page = 1;
+            }
+
+            DisplayPage();
+        }
+
+        private void optionButton2_Click(object sender, EventArgs e)
+        {
+            //Check page, then flip based on if you selected Option 2
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 7;
+            }
+            else if (page == 3)
+            {
+                page = 113;
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 5)
+            {
+                page = 113;
+            }
+            else if (page == 6)
+            {
+                page = 113;
+            }
+            else if (page == 7)
+            {
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.Next(1, 101);
+
+                if (randomNumber >= 50)
+                {
+                    page = 9;
+                }
+
+                else
+                {
+                    page = 10;
+                }
+            }
+            else if (page == 8)
+            {
+                page = 113;
+            }
+            else if (page == 9)
+            {
+                page = 113;
+            }
+            else if (page == 10)
+            {
+                page = 12;
+            }
+            else if (page == 11)
+            {
+                page = 14;
+            }
+            else if (page == 12)
+            {
+                page = 113;
+            }
+            else if (page == 13)
+            {
+                page = 113;
+            }
+            else if (page == 14)
+            {
+                page = 113;
+            }
+
+            DisplayPage();
+        }
+
+        public void DisplayPage()
+        {
+            //Display text, options, images and sounds based on the current page
+            switch (page)
+            {
+                case 1:
+                    imageBox.Image = Properties.Resources.houseExterior;
+
+                    outputLabel.Text = "You get up to go to work. You are tired. Do you get a coffee to feel more awake?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 2:
+                    soundPlayer = new SoundPlayer(Properties.Resources.coffeePouring_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.houseExterior;
+
+                    outputLabel.Text = "You make yourself a coffee and leave the house. Do you take your car or public transportation?";
+                    optionLabel1.Text = "Car";
+                    optionLabel2.Text = "Public transportation";
+                    break;
+                case 3:
+                    soundPlayer = new SoundPlayer(Properties.Resources.snoring_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.houseExterior;
+
+                    outputLabel.Text = "You call in sick and go back to bed. Another day of relaxation... Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 4:
+                    soundPlayer = new SoundPlayer(Properties.Resources.carEngineStart_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.trafficJam;
+
+                    outputLabel.Text = "You get stuck in traffic thanks to road works. Do you take a detour or wait?";
+                    optionLabel1.Text = "Deviate";
+                    optionLabel2.Text = "Wait";
+                    break;
+                case 5:
+                    soundPlayer = new SoundPlayer(Properties.Resources.carHorns_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.officeBuilding;
+
+                    outputLabel.Text = "You take a detour and get to work on time. The boss is pleased you've arrived on schedule. Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 6:
+                    soundPlayer = new SoundPlayer(Properties.Resources.carHorns_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.officeBuilding;
+
+                    outputLabel.Text = "You arrive late for work. The boss isn't happy about it. Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 7:
+                    imageBox.Image = Properties.Resources.busTrainStation;
+
+                    outputLabel.Text = "You walk over to a junction and see a bus station and a train station. Which do you choose?";
+                    optionLabel1.Text = "Bus";
+                    optionLabel2.Text = "Train";
+                    Refresh();
+                    break;
+                case 8:
+                    soundPlayer = new SoundPlayer(Properties.Resources.failSoundEffect_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.busPulledOver;
+
+                    outputLabel.Text = "The bus gets a flat on the highway. Looks like you're walking the rest of the way... Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 9:
+                    soundPlayer = new SoundPlayer(Properties.Resources.trainHorn_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.officeBuilding;
+
+                    outputLabel.Text = "The train takes you straight to work. You're early! Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 10:
+                    soundPlayer = new SoundPlayer(Properties.Resources.trainHorn_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.viaTrain;
+
+                    outputLabel.Text = "The train breaks down en-route. The conductor asks for assistance in fixing the train. Do you help or not?";
+                    optionLabel1.Text = "Assist";
+                    optionLabel2.Text = "Sit tight";
+                    break;
+                case 11:
+                    imageBox.Image = Properties.Resources.viaTrain;
+
+                    outputLabel.Text = "The conductor leads you to the power car. The engine seems to not be working. What do you try doing?";
+                    optionLabel1.Text = "Restart the engine";
+                    optionLabel2.Text = "Find broken parts";
+                    break;
+                case 12:
+                    imageBox.Image = Properties.Resources.officeBuilding;
+
+                    outputLabel.Text = "Another passenger assists with fixing the train. You arrive for work late, but your boss is understanding about the incident. Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 13:
+                    soundPlayer = new SoundPlayer(Properties.Resources.trainPassingWithEngine_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.officeBuilding;
+
+                    outputLabel.Text = "You restart the engine, and it works fine. It must've shut off somehow. You arrive for work on time. Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 14:
+                    soundPlayer = new SoundPlayer(Properties.Resources.trainPassingWithEngine_wav);
+                    soundPlayer.Play();
+
+                    imageBox.Image = Properties.Resources.officeBuilding;
+
+                    outputLabel.Text = "Nothing seems to be broken. So you restart the engine. And its running again. You eventually arrive at work... on time, thankfully. Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    break;
+                case 113:
+                    outputLabel.Text = "Thanks for playing";
+                    optionLabel1.Text = "";
+                    optionLabel2.Text = "";
+                    optionButton1.Enabled = false;
+                    optionButton2.Enabled = false;
+                    Refresh();
+
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
